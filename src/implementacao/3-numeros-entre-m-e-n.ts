eaaae49c5aac788f687ae8f1f852e5934a67ae6d
Marvyson Cardoso <marvysonprog@gmail.com>
2024-09-10 13:48:16 -0300

    feat: Resposta do Exercicio3

--- src/implementacao/3-numeros-entre-m-e-n.ts
+++ src/implementacao/3-numeros-entre-m-e-n.ts
@@ -1,3 +1,24 @@
 function mostrarNumerosEntreMeN() {
-    return null;
+    const readline = require('readline');
+    const teclado = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    })
+
+    teclado.question("Digite o primeiro numero: ", (primeiroNumero: string) => {
+        let numero1 = parseInt(primeiroNumero);
+        teclado.question("Digite o segundo numero: ", (segundoNumero: string) => {
+            let numero2 = parseInt(segundoNumero);
+
+            if( numero1 < numero2){
+                for(let i = numero1 ; i <= numero2 ; i++){
+                    console.log(i);
+                }
+            } else {
+                console.log("O primeiro valor precisa ser menor do que o segundo.")
+            }
+            teclado.close();
+        })
+    })
 }
+mostrarNumerosEntreMeN();
