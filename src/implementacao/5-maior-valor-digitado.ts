a518f64a5fd12d82ccd259ca3f8d79c4119b1d72
Marvyson Cardoso <marvysonprog@gmail.com>
2024-09-10 15:40:40 -0300

    feat: Resposta do Exercicio5 parte1

--- src/implementacao/5-maior-valor-digitado.ts
+++ src/implementacao/5-maior-valor-digitado.ts
@@ -1,3 +1,17 @@
 function mostrarMaiorValor() {
-    return null;
+    const readline = require('readline');
+    const teclado = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    })
+
+    teclado.question("Digite um numero: ", (primeiroNumero: string) => {
+        let numero1 = parseInt(primeiroNumero);
+        
+        for( let i = 0 ; i < numero1 ; i++){
+            console.log();
+        }
+        teclado.close();
+    })
 }
+mostrarMaiorValor();
