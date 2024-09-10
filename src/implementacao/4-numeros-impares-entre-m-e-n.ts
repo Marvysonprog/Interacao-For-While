d6c984d24f6b0d1434621ad0158fdcff247a6d27
Marvyson Cardoso <marvysonprog@gmail.com>
2024-09-10 15:21:12 -0300

    feat: Respostado do Exercicio4

--- src/implementacao/4-numeros-impares-entre-m-e-n.ts
+++ src/implementacao/4-numeros-impares-entre-m-e-n.ts
@@ -1,3 +1,28 @@
 function mostrarImparesEntreMeN() {
-    return null;
+  const readline = require("readline");
+  const teclado = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  teclado.question("Digite o primeiro numero: ", (primeiroNumero: string) => {
+    let numero1 = parseInt(primeiroNumero);
+    teclado.question("Digite o segundo numero: ", (segundoNumero: string) => {
+      let numero2 = parseInt(segundoNumero);
+
+      if (numero1 < numero2) {
+        for (let i = numero1; i <= numero2; i++) {
+          if (i % 2 != 0) {
+            console.log(i);
+          }
+        }
+      } else {
+        console.log(
+          "O primeiro numero digitado precisa ser menor do que o segundo."
+        );
+      }
+      teclado.close();
+    });
+  });
 }
+mostrarImparesEntreMeN();
