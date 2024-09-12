6b4590c945775b9aa703fa173ed1e10007eb4086
Marvyson Cardoso <marvysonprog@gmail.com>
2024-09-12 14:10:41 -0300

    feat: Resposta do Exercicio9

--- src/implementacao/9-media-de-3-alunos.ts
+++ src/implementacao/9-media-de-3-alunos.ts
@@ -1,3 +1,27 @@
+const readl = require("readline-sync");
+
 function mediaNotas() {
-    return null;
+  for (let i = 1; i <= 3; i++) {
+    const nomeAluno = readl.question("Digite o nome do aluno: ");
+    const primeiraNota = parseFloat(
+      readl.question("Digite a primeira nota de " + nomeAluno + ": ")
+    );
+    const segundaNota = parseFloat(
+      readl.question("Digite a segunda nota de " + nomeAluno + ": ")
+    );
+    const terceiraNota = parseFloat(
+      readl.question("Digite a terceira nota de " + nomeAluno + ": ")
+    );
+
+    let media: number = (primeiraNota + segundaNota + terceiraNota) / 3;
+
+    if (media >= 7 && media <= 10) {
+      console.log(nomeAluno + " Foi Aprovado!");
+    } else if (media > 4 && media < 7) {
+      console.log(nomeAluno + " Vai para Recuperação!");
+    } else {
+      console.log(nomeAluno + " Foi Reprovado!");
+    }
+  }
 }
+mediaNotas();
