c607dd4231ae7d1240d27a033769d6ad55f30592
Marvyson Cardoso <marvysonprog@gmail.com>
2024-09-12 15:25:34 -0300

    feat: Resposta do Exercicio10

--- src/implementacao/10-tabela-polegadas-para-cm.ts
+++ src/implementacao/10-tabela-polegadas-para-cm.ts
@@ -1,3 +1,10 @@
 function tabelaConversao() {
-    return null;
+  const polegada: number = 2.54;
+
+  for (let i = 1; i <= 20; i++) {
+    let centimetro: number = polegada * i;
+
+    console.table({ [`${i} polegadas`]: centimetro + " centimetros" });
+  }
 }
+tabelaConversao();
