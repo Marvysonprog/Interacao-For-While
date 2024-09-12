a33073cedad3696ec120583aeb5f6219cc1aa354
Marvyson Cardoso <marvysonprog@gmail.com>
2024-09-12 10:02:51 -0300

    feat: Resposta do Exercicio6 Final

--- src/implementacao/6-segundo-maior-valor.ts
+++ src/implementacao/6-segundo-maior-valor.ts
@@ -1,11 +1,22 @@
-const teclado = require("readline-sync")
+const teclado = require("readline-sync");
 
 function mostrarSegundoMaiorValor() {
+  let maiorValor: number = 0;
+  let segundoMaiorValor: number = 0;
 
-    let maiorValor: number = 0;
-    let segundoMaiorValor: number = 0;
-    
-    const numero = parseInt(teclado.question("Digite a quantidade de numeros: "));
-    
-    
+  const quantidade = parseInt(
+    teclado.question("Digite a quantidade de numeros: ")
+  );
+
+  for (let i = 0; i < quantidade; i++) {
+    let numero = parseInt(teclado.question("Digite um numero: "));
+
+    if (numero > maiorValor) {
+      let temp = maiorValor;
+      maiorValor = numero;
+      segundoMaiorValor = temp;
+    }
+  }
+  console.log(segundoMaiorValor);
 }
+mostrarSegundoMaiorValor();
