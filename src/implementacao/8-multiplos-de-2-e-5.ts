26ee11e4181f1d794007fff77cb9d1fa7e6b3d0f
Marvyson Cardoso <marvysonprog@gmail.com>
2024-09-12 11:52:21 -0300

    feat: Resposta do Exercicio8

--- src/implementacao/8-multiplos-de-2-e-5.ts
+++ src/implementacao/8-multiplos-de-2-e-5.ts
@@ -1,3 +1,24 @@
+const tc = require("readline-sync");
+
 function analiseMultiplos() {
-    return null;
+  let multDoiseCinco: number = 0;
+  let multDoiseTres: number = 0;
+
+  while (true) {
+    let valor = parseInt(tc.question("Digite um valor: "));
+
+    if (valor % 2 == 0 && valor % 5 == 0) {
+      multDoiseCinco += 1;
+    } else if (valor % 2 == 0 && valor % 3 == 0) {
+      multDoiseTres += 1;
+    }
+ 
+    if (valor == -1) {
+      console.log(`Foram digitados ${multDoiseCinco} multiplos de 2 e 5.`);
+      console.log(`Foram digitados ${multDoiseTres} multiplos de 2 e 3.`);
+      console.log("Programa encerrado.");
+      break;
+    }
+  }
 }
+analiseMultiplos();
