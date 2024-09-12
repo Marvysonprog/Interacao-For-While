a4e25453700255b75e0183724e628b96debf0fc9
Marvyson Cardoso <marvysonprog@gmail.com>
2024-09-12 11:00:13 -0300

    feat: Resposta do Exercicio7

--- src/implementacao/7-soma-intervalo-m-a-n.ts
+++ src/implementacao/7-soma-intervalo-m-a-n.ts
@@ -1,3 +1,18 @@
+const rl = require("readline-sync");
+
 function somarNumerosEntreMeN() {
-    return null;
+  while (true) {
+    let numero1 = parseInt(rl.question("Digite o primeiro numero: "));
+    let numero2 = parseInt(rl.question("Digite o segundo numero: "));
+    let soma: number = 0;
+    for (let i = numero1; i <= numero2; i++) {
+      soma += i;
+    }
+    if (numero1 > numero2) {
+        console.log("Programa encerrado, pois o primeiro numero precisa ser menor do que o segundo.")
+      break;
+    }
+    console.log(soma);
+  }
 }
+somarNumerosEntreMeN();
